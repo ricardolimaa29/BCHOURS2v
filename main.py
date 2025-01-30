98af1c3b2c396b300cf0256d4f4678c3f18e5767
Ricardo Rodrigues Lima <147922620+ricardolimaa29@users.noreply.github.com>
2025-01-30 00:24:21 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+### Interface: "Flet" - Python com foco em simplicidade e elegancia
+### Funções asyncronas para que o sistema rode em tempo real
+### Banco de dados: MySql 2008  - Python = "pyodbc"
+### Gerar PDF: PDF2, pyPDF
+
+
+
+# import flet - Interface
+# import pyodbc - Conexao com o BANCO
+# import datetime - Função
+# import async - Função
+# import pyPDF, PDF2 (from reportlab.pdfgen import canvas)
+# import request
+
+# import pyinstaller - Cria executavel
+
+
+# campo manipulado: "Nome do funcionario" ft.TextField
+# botao de gerar informações do funcionario: ft.elevateButton
+# campo de destino: "Seleciona o destino para salvar"
+# Apresentar informações geradas pelo banco: ft.Text, ft.DataTable, ft.ListTile
+# Botao de gerar aquivo PDF,TXT: IMPORT CANVAS*
+
+ 
+
+
+#### Desejavel: enviar por email, ou whats ( email da TI: tecnologia.lin@friboi.com.br)
+####                
